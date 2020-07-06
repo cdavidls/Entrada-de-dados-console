d87ef6ee7f0178eb4f678428e9799905d50557db
David <dcicero493@gmail.com>
2020-07-06 14:05:12 -0300

    Entrada de dados console

---
+++ EntradaDados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Informe a sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bom dia, " + nome + " você tem " + idade + " anos.");
+
+            Console.ReadKey();
+        }
+    }
+}
